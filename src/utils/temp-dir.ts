358addf49db485cccd4a2f3491aa3a4747437d56
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-11-30 17:38:52 +0100

    improve validation of input

--- src/utils/temp-dir.ts
+++ src/utils/temp-dir.ts
@@ -8,11 +8,11 @@ import { v4 as uuidv4 } from 'uuid';
 import { logger } from './logger';
 
 export function createTempDirectory() {
-  const p = path.join(__dirname, '../../examples/');
+  // const p = path.join(__dirname, '../../examples/');
+  // // @ts-ignore
+  // return fs.mkdtempSync(path.join(p, uuidv4()));
   // @ts-ignore
-  return fs.mkdtempSync(path.join(p, uuidv4()));
-  // @ts-ignore
-  // return fs.mkdtempSync(path.join(os.tmpdir(), uuidv4()));
+  return fs.mkdtempSync(path.join(os.tmpdir(), uuidv4()));
 }
 
 export function removeTempDirectory(tmpDir: string) {
