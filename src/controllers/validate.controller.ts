e22b536d885d3785721123424ae969529b017125
David <didicp4@gmail.com>
2022-01-07 22:33:59 +0000

    refactor: remove checks on validate controller

--- src/controllers/validate.controller.ts
+++ src/controllers/validate.controller.ts
@@ -4,10 +4,7 @@ import { Controller } from '../interfaces';
 
 import { ParserService } from '../services/parser.service';
 
-import { ProblemException } from '../exceptions/problem.exception';
 import { prepareParserConfig, tryConvertToProblemException } from '../utils/parser';
-
-const BAD_REQUEST = 'Bad Request';
 
 /**
  * Controller which exposes the Parser functionality, to validate the AsyncAPI document.
@@ -19,25 +16,8 @@ export class ValidateController implements Controller {
 
   private async validate(req: Request, res: Response, next: NextFunction) {
     try {
-      const asyncapi = req.body?.asyncapi;
-      if (!asyncapi) {
-        return next(new ProblemException({
-          type: 'null-or-falsey-document',
-          title: BAD_REQUEST,
-          status: 400,
-          detail: 'The "asyncapi" field in the request payload is required.',
-        }));
-      } else if (typeof asyncapi !== 'string' && typeof asyncapi !== 'object') {
-        return next(new ProblemException({
-          type: 'invalid-document-type',
-          title: BAD_REQUEST,
-          status: 400,
-          detail: 'The "asyncapi" field must be a string or object.',
-        }));
-      }
-
       const options = prepareParserConfig(req);
-      await this.parserService.parse(asyncapi, options);
+      await this.parserService.parse(req.body?.asyncapi, options);
 
       res.status(204).end();
     } catch (err: unknown) {
