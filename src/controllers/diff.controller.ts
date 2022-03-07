9ea0595aca1ab86ac95526c9cda5531f9c67dd8b
ritik307 <ritikpr307@gmail.com>
2022-03-07 23:05:47 +0530

    made requested changes

--- src/controllers/diff.controller.ts
+++ src/controllers/diff.controller.ts
@@ -1,6 +1,4 @@
 import { NextFunction, Request, Response, Router } from 'express';
-import Ajv from 'ajv';
-import { ProblemException } from '../exceptions/problem.exception';
 import { Controller} from '../interfaces';
 
 import { documentValidationMiddleware } from '../middlewares/document-validation.middleware';
@@ -9,49 +7,15 @@ import { DiffService } from '../services/diff.service';
 
 export class DiffController implements Controller {
     public basepath = '/diff';
-    private ajv: Ajv;
     diffService: DiffService;
     private async difference(req: Request, res: Response, next: NextFunction) {
-        try {
-            await this.validateDiffDocumentsPath(req.body);
-        } catch (err) {
-            return next(new ProblemException({
-                type: 'invalid-diff-documents-path',
-                title: 'Invalid diff documents path',
-                status: 400,
-                detail: err.message,
-                }));
-        }
-        const { asyncapi, other } = req.body;
-        const output = await (await this.diffService.diff(asyncapi, other)).getOutput();
+        const { asyncapis } = req.body;
+        const output = await (await this.diffService.diff(asyncapis[0],asyncapis[1])).getOutput();
         console.log(output);
         res.status(200).json(output);
     }
-    private async validateDiffDocumentsPath(body: any) {
-        const validate = this.ajv.compile({
-            type: 'object',
-            properties: {
-                asyncapi: {
-                    type: 'string',
-                },   
-                other: {
-                    type: 'string',
-                },  
-            },
-            required: ['asyncapi', 'other'],
-        });
-        const valid = validate(body);
-        if (!valid) {
-            throw new Error(`Invalid request body: ${JSON.stringify(validate.errors)}`);
-        }
-    }
+    
     public boot(): Router {
-        this.ajv = new Ajv({
-          inlineRefs: true,
-          allErrors: true,
-          schemaId: 'id',
-          logger: false,
-        });
         const router = Router();
     
         router.post(
