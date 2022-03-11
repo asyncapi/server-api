c74292755795b9a435932cd23b1b0140124cb9a3
everly <everlyprecia@gmail.com>
2022-03-11 23:09:08 +0530

    Serve docs from yaml

---
+++ src/controllers/docs.controller.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response, Router } from 'express';
+
+import redoc from 'redoc-express';
+
+import { Controller } from '../interfaces';
+
+/**
+ * Controller which exposes the Parser functionality, to validate the AsyncAPI document.
+ */
+export class DocsController implements Controller {
+  public basepath = '/docs';
+
+  private async docs(req: Request, res: Response, next: NextFunction) {
+    try { 
+
+      await redoc({
+        title: 'API Docs',
+        specUrl: '.../openapi.yaml'
+      });
+
+      res.status(204).end();
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  public boot(): Router {
+    const router = Router();
+
+    router.post(
+      `${this.basepath}`,
+      this.docs.bind(this)
+    );
+
+    return router;
+  }
+}
