8c6565ecbb527e808eff21f43b99b0b95e4f28dd
Everly Precia Suresh <77877486+everly-gif@users.noreply.github.com>
2022-03-15 00:40:20 +0530

    Update docs.controller.ts

--- src/controllers/docs.controller.ts
+++ src/controllers/docs.controller.ts
@@ -5,30 +5,25 @@ import redoc from 'redoc-express';
 import { Controller } from '../interfaces';
 
 export class DocsController implements Controller {
+
   public basepath = '/docs';
-
-  private async docs(req: Request, res: Response, next: NextFunction) {
-    try { 
-
-      await redoc({
-        title: 'API Docs',
-        specUrl: '.../openapi.yaml'
-      });
-
-      res.status(204).end();
-    } catch (err) {
-      return next(err);
-    }
-  }
 
   public boot(): Router {
     const router = Router();
+    
+    router.get('/docs/openapi.yaml', (req, res) => {
+      res.sendFile('.../openapi.yaml');
+    });
 
     router.post(
       `${this.basepath}`,
-      this.docs.bind(this)
+      redoc({
+        title: 'API Docs',
+        specUrl: 'docs/openapi.yaml'
+      })
     );
 
+    
     return router;
   }
 }
