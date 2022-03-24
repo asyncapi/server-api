b73969116b75d177df46d7c9b72cb052be4e73a1
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-03-24 15:07:02 +0100

    fix circular references

--- src/controllers/bundler.controller.ts
+++ src/controllers/bundler.controller.ts
@@ -2,18 +2,19 @@ import { NextFunction, Request, Response, Router } from 'express';
 import { ProblemException } from '../exceptions/problem.exception';
 import { Controller } from '../interfaces';
 import bundler from '@asyncapi/bundler';
-import Document from '@asyncapi/bundler/lib/document.js';
 import { documentValidationMiddleware } from '../middlewares/document-validation.middleware';
 import path from 'path';
 
 export class BundlerController implements Controller {
   public basepath = '/bundle';
   private async bundle(req: Request, res: Response, next: NextFunction) {
-    const asyncapis: Array<string> = req.body.asyncapis || [req.body.asyncapi];
+    const asyncapis: Array<string> = req.body.asyncapis;
+    const base = req.body.base;
+
     try {
-      const document = await bundler.bundle(asyncapis, { base: path.dirname(asyncapis[0]) });
-      const doc = new Document(document).json();
-      res.status(200).json(doc);
+      const document = await bundler(asyncapis, { base });
+      const bundled = document.json();
+      res.status(200).json({ bundled });
     } catch (err) {
       return next(new ProblemException({
         type: 'internal-bundler-error',
--- src/controllers/tests/bundler.controller.test.ts
+++ src/controllers/tests/bundler.controller.test.ts
@@ -8,7 +8,7 @@ describe('BundlerController', () => {
   describe('[POST] /bundle', () => {
     it('should bundle files', async () => {
       const app = new App([new BundlerController()]);
-      const response = await request(app.getServer())
+      await request(app.getServer())
         .post('/v1/bundle')
         .send({
           asyncapis: [{
@@ -17,30 +17,66 @@ describe('BundlerController', () => {
               title: 'Test Service',
               version: '1.0.0',
             },
+            servers: {},
             channels: {
-              'test-channel': {
-                messages: {
-                  'test-message': {
+              'test-channel-2': {
+                publish: {
+                  message: {
                     payload: {
                       type: 'object',
                     },
                   },
-                },
+                }
               },
             },
           }],
           base: {
             asyncapi: '2.2.0',
             info: {
-              title: 'Test Service',
-              version: '1.0.0',
+              title: 'Merged test service',
+              version: '1.2.0',
             },
-            channels: {},
+            servers: {},
+            channels: {
+              'test-channel-1': {
+                publish: {
+                  message: {
+                    payload: {
+                      type: 'object',
+                    },
+                  },
+                }
+              },
+            },
           }
-        })
-        .set('Accept', 'application/json');
-      expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-      expect(response.status).toBe(200);
+        }).
+        expect(200, {
+          bundled: {
+            asyncapi: '2.2.0',
+            info: { title: 'Merged test service', version: '1.2.0' },
+            servers: {},
+            channels: {
+              'test-channel-1': {
+                publish: {
+                  message: {
+                    payload: {
+                      type: 'object',
+                    },
+                  },
+                }
+              },
+              'test-channel-2': {
+                publish: {
+                  message: {
+                    payload: {
+                      type: 'object',
+                    },
+                  },
+                }
+              },
+            }
+          }
+        });
     });
   });
 });
