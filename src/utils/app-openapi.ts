e22571482f4479bba0f0118bbef4ab19c9712d28
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-05-23 11:38:04 +0200

    fix: update deps

--- src/utils/app-openapi.ts
+++ src/utils/app-openapi.ts
@@ -16,6 +16,7 @@ export async function getAppOpenAPI(): Promise<any> {
   
   const openaAPI = await fs.readFile(path.join(__dirname, '../../openapi.yaml'), 'utf-8');
   parsedOpenAPI = YAML.load(openaAPI);
+  parsedOpenAPI.components.schemas.AsyncAPIDocument.oneOf = { type: ['string', 'object'] };
   const refParser = new $RefParser;
   await refParser.dereference(parsedOpenAPI);
 
