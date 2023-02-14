2214c3b8678d22180880d7a0e632d5f6d9ac662f
acethecreator <devaze007@gmail.com>
2023-02-14 11:26:26 +0100

    .

--- src/controllers/tests/share.controller.test.ts
+++ src/controllers/tests/share.controller.test.ts
@@ -5,93 +5,37 @@ import { App } from '../../app';
 
 import {ShareController} from '../share.controller';
 
-const asyncFile = {
+const validAsyncFile = {
   asyncapi: {
-    asyncapi: '2.4.0',
+    asyncapi: '2.5.0',
     info: {
-      title: 'Building MQTT API',
+      title: 'Account Service',
       version: '1.0.0',
-      description: 'First prototype of building API using mosquitto broker',
-      license: {
-        name: 'Apache 2.0',
-        url: 'https://www.apache.org/licenses/LICENSE-2.0',
-      },
-    },
-    servers: {
-      development: {
-        url: 'test.mosquitto.org',
-        protocol: 'mqtt',
-        description: 'Mosquitto broker for development',
-        variables: {
-          port: {
-            description:
-              'Secure connection (TLS) is available through port 8883.',
-            default: '1883',
-          },
-        },
-      },
+      description: 'This service is in charge of processing user signups',
     },
     channels: {
-      unitBalanceRequester: {
-        description:
-          'This topic publishes an event demanding building unit balance',
+      'user/signedup': {
         subscribe: {
-          summary:
-            'This triggers a request action for each building about the balance',
-          operationId: 'BalanceRequester',
           message: {
-            $ref: '#/components/messages/lightBalanceRequester',
-          },
-        },
-      },
-      unitBalanceResponder: {
-        description: 'This topic publishes event for building unit balance',
-        publish: {
-          summary: 'This triggers a balance event for building units measured',
-          operationId: 'BalanceResponder',
-          message: {
-            $ref: '#/components/messages/lightMeasured',
-          },
-        },
-      },
-      unitBalanceUpdater: {
-        description: 'This topic publishes event that updates unit balance',
-        subscribe: {
-          summary: 'This triggers a balance event for building units measured',
-          operationId: 'UnitBalanceUpdater',
-          message: {
-            $ref: '#/components/messages/lightMeasured',
+            $ref: '#/components/messages/UserSignedUp',
           },
         },
       },
     },
     components: {
       messages: {
-        lightBalanceRequester: {
-          name: 'lightBalanceRequester',
-          title: 'Light Balance Requester',
-          summary: 'Request balance from buildings',
-          contentType: 'application/json',
-          payload: {
-            type: 'string',
-          },
-        },
-        lightMeasured: {
-          name: 'lightMeasured',
-          title: 'Light measured',
-          summary:
-            'Inform about environmental lighting conditions of a particular streetlight.',
-          contentType: 'application/json',
+        UserSignedUp: {
           payload: {
             type: 'object',
             properties: {
-              buildingID: {
-                type: 'integer',
-                description: 'Building unique indentifier',
+              displayName: {
+                type: 'string',
+                description: 'Name of the user',
               },
-              unit: {
-                type: 'integer',
-                description: 'Number of unit remaining from building',
+              email: {
+                type: 'string',
+                format: 'email',
+                description: 'Email of the user',
               },
             },
           },
@@ -100,6 +44,28 @@ const asyncFile = {
     },
   },
 };
+
+const invalidAsyncApiFile = {
+  asyncapi: {
+    asyncapi: '2.5.0',
+    info: {
+      title: 'Account Service',
+      version: '1.0.0',
+      description: 'This service is in charge of processing user signups',
+    },
+    channels: {
+      'user/signedup': {
+        subscribe: {
+          message: {
+            $ref: '#/components/messages/UserSignedUp',
+          },
+        },
+      },
+    },
+  },
+};
+
+const invalidUUID = '63ybdy474'; 
 
 beforeAll(async () => {
   await mongoose.connect(
@@ -115,8 +81,29 @@ describe('ShareController', () => {
 
       return request(app.getServer())
         .post('/v1/share')
-        .send(asyncFile)
+        .send(validAsyncFile)
         .expect(201);
+    });
+
+    it('should return an error when an invalid spec is passed', async () => {
+      const app = new App([new ShareController()]);
+      await app.init();
+
+      return request(app.getServer())
+        .post('/v1/share')
+        .send(invalidAsyncApiFile)
+        .expect(422);
+    });
+  });
+
+  describe('[GET] /share/id', () => {
+    it('it should return an invalid UUID ID', async () => {
+      const app = new App([new ShareController]);
+      await app.init();
+
+      return request(app.getServer())
+        .get(`/share/${invalidUUID}`)
+        .expect(404);
     });
   });
 });
