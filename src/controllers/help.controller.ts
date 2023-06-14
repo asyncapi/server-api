3d0c0c0d8cccdb8a13ff8aa2712c88908a281920
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2023-06-15 02:10:05 +0530

    [feat] : Added functionality of fetching data from github repo and make changes in commands.md file

--- src/controllers/help.controller.ts
+++ src/controllers/help.controller.ts
@@ -1,16 +1,21 @@
 import { Router, Request, Response } from 'express';
 import { Controller } from '../interfaces';
+import axios from 'axios';
+import MarkdownIt from 'markdown-it';
 
-const helpData = {
-    command1: {
-        info: 'Details about command1. It has subcommand1 and subcommand2',
-        subcommand1: 'Details about command1 > subcommand1',
-        subcommand2: 'Details about command1 > subcommand2'
-    },
-    command2: {
-        info: 'Details about command2. It has subcommand1 and subcommand2',
-        subcommand1: 'Details about command2 > subcommand1',
-        subcommand2: 'Details about command2 > subcommand2'
+const md = new MarkdownIt();
+
+const fetchCommands = async (user, repo) => {
+    try {
+        const url = `https://api.github.com/repos/${user}/${repo}/contents/commands.md`;
+        const response = await axios.get(url, {
+            headers: {
+                'Accept': 'application/vnd.github.v3.raw',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching commands: ${error}`);
     }
 };
 
@@ -20,33 +25,41 @@ export class HelpController implements Controller {
     public boot(): Router {
         const router: Router = Router();
 
-        router.get(this.basepath, (req: Request, res: Response) => {
-            const command = req.body.command.trim();
-
-            if (!command) {
-                return res.status(400).json({ message: 'Command parameter is required' });
+        router.get(this.basepath, async (req: Request, res: Response) => {
+            const command = req.body.command ? req.body.command.trim() : null;
+        
+            const commandsMarkdown = await fetchCommands('princerajpoot20', 'api_endpoint');
+        
+            if (!commandsMarkdown) {
+                return res.status(500).json({ message: 'Error fetching help information' });
             }
-
-            const [mainCommand, subCommand] = command.split(' ');
-
-            // Check if the main command exists
-            if (!helpData[mainCommand]) {
+        
+            if (!command) {
+                // Return information for only the list of commands
+                const endOfList = commandsMarkdown.indexOf('##', commandsMarkdown.indexOf('<!-- commands -->'));
+                const commandsListSection = commandsMarkdown.substring(0, endOfList).trim();
+                
+                if (!commandsListSection) {
+                    return res.status(404).json({ message: 'Commands list not found' });
+                }
+                
+                const htmlContent = md.render(commandsListSection);
+                return res.send(htmlContent);
+            }
+        
+            const sections = commandsMarkdown.split('##');
+            const commandSection = sections.find(section => section.trim().startsWith(`\`${command}\``));
+        
+            if (!commandSection) {
                 return res.status(404).json({ message: 'Help information not found' });
             }
-
-            // If a subcommand is specified, check if it exists
-            if (subCommand) {
-                const subCommandInfo = helpData[mainCommand][subCommand];
-                if (!subCommandInfo) {
-                    return res.status(404).json({ message: 'Help information not found' });
-                }
-                return res.json({ info: subCommandInfo });
-            }
-
-            // If no subcommand is specified, return the main command info
-            const mainCommandInfo = helpData[mainCommand].info;
-            return res.json({ info: mainCommandInfo });
-        });
+        
+            const htmlContent = md.render(`##${commandSection}`);
+        
+            return res.send(htmlContent);
+        });        
+        
+        
 
         return router;
     }
