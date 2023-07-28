993d7ce801cf45d5bff4e182e5a55c1b67354445
Prince Rajpoot <prince.rajpoot.20@gmail.com>
2023-07-28 23:45:09 +0530

    change response to json formate

--- src/controllers/help.controller.ts
+++ src/controllers/help.controller.ts
@@ -1,19 +1,17 @@
 import { Router, Request, Response } from 'express';
 import { Controller } from '../interfaces';
 import axios from 'axios';
-import MarkdownIt from 'markdown-it';
-
-const md = new MarkdownIt();
+import yaml from 'js-yaml';
 
 const fetchCommands = async (user, repo) => {
     try {
-        const url = `https://api.github.com/repos/${user}/${repo}/contents/docs/usage.md`;
+        const url = `https://api.github.com/repos/${user}/${repo}/contents/openapi.yaml`;
         const response = await axios.get(url, {
             headers: {
                 'Accept': 'application/vnd.github.v3.raw',
             },
         });
-        return response.data;
+        return yaml.load(response.data);
     } catch (error) {
         console.error(`Error fetching commands: ${error}`);
     }
@@ -27,43 +25,67 @@ export class HelpController implements Controller {
 
         router.get(/^\/help(\/.*)?$/, async (req: Request, res: Response) => {
             const commands = req.params[0] ? req.params[0].split('/').filter(cmd => cmd.trim()) : [];
+            const openapiSpec: any = await fetchCommands('asyncapi', 'server-api');
 
-            const commandsMarkdown = await fetchCommands('asyncapi', 'cli');
-
-            if (!commandsMarkdown) {
+            if (!openapiSpec) {
                 return res.status(500).json({ message: 'Error fetching help information' });
             }
 
             if (commands.length === 0) {
-                // Return information for only the list of commands
-                const startOfList = commandsMarkdown.indexOf('<!-- commands -->');
-                const endOfList = commandsMarkdown.indexOf('##', startOfList + 1); // Find the next markdown header
-                
-                // Extract the commands list
-                let commandsListSection = commandsMarkdown.substring(startOfList, endOfList !== -1 ? endOfList : undefined).trim();
-                
-                if (!commandsListSection) {
-                    return res.status(404).json({ message: 'Commands list not found' });
+                const routes = Object.keys(openapiSpec.paths).map(path => ({ command: path.replace(/^\//, ''), url: `${this.basepath}${path}` }));
+                return res.json(routes);
+            }
+
+            const pathKeys = Object.keys(openapiSpec.paths);
+            const matchedPathKey = pathKeys.find(pathKey => {
+                const pathParts = pathKey.split('/').filter(part => part !== '');
+                if (pathParts.length !== commands.length) {
+                    return false;
                 }
-                // Replace <!-- commands --> with ## Commands
-                commandsListSection = commandsListSection.replace('<!-- commands -->', '## Commands');
-
-                const htmlContent = md.render(commandsListSection);
-                return res.send(htmlContent);
-            }                                              
-
-            const sections = commandsMarkdown.split('##');
-            const commandString = commands.join(' ').toLowerCase();
-            const commandSection = sections.find(section => section.trim().toLowerCase().startsWith(`\`${commandString}\``));
-
-            if (!commandSection) {
+                for (let i = 0; i < pathParts.length; i++) {
+                    const pathPart = pathParts[i];
+                    const command = commands[i];
+                    if (pathPart !== command && !pathPart.startsWith('{')) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+            
+            if (!matchedPathKey) {
                 return res.status(404).json({ message: 'Help information not found' });
             }
 
-            const htmlContent = md.render(`##${commandSection}`);
-            return res.send(htmlContent);
+            const pathInfo = openapiSpec.paths[matchedPathKey];
+            const method = commands.length > 1 ? 'get' : 'post';
+            const operationDetails = pathInfo[method];
+
+            if (!operationDetails) {
+                return res.status(404).json({ message: 'Help information not found' });
+            }
+
+            const { requestBody } = operationDetails;
+
+            let requestBodyComponent = {};
+
+            if (requestBody && requestBody.content && requestBody.content['application/json']) {
+                const { $ref } = requestBody.content['application/json'].schema;
+                const componentKey = $ref.replace('#/components/schemas/', '');
+                requestBodyComponent = openapiSpec.components.schemas[componentKey];
+            }
+
+            const responseObject = {
+                command: matchedPathKey,
+                method: method.toUpperCase(),
+                summary: operationDetails.summary || '',
+                description: operationDetails.description || '',
+                parameters: operationDetails.parameters || [],
+                requestBody: requestBodyComponent,
+            };
+            
+            return res.json(responseObject);
         });
-        
+
         return router;
     }
 }
